e5a5e98cce29e6099b93a4a458948fd8848cc879
pavaaaaaaaan <pavaaaaaaaan@gmail.com>
2018-09-18 11:19:54 +0530

    added test.s file

---
+++ test.sh
@@ -0,0 +1,18 @@
+1#!/bin/bash
+ 
+echo "making foss folder"
+ mkdir foss
+
+echo "Going to foss folder"
+cd foss
+
+echo "creating test_file.txt"
+touch test_file.txt
+
+echo ""
+echo "showing output of ls:"
+ls
+
+echo ""
+echo "updating linux repos"
+sudo apt-get update
